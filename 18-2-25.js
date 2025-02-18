2b49398b3af5af41638532c5a6a094fa231652e9
Nayani_Karthik <nayanikarthik961@gmail.com>
2025-02-18 09:55:17 +0530

    i love you

--- 18-2-25.js
+++ 18-2-25.js
@@ -3,6 +3,6 @@ const server = http.createServer((req, res) => {
   res.write("hello darling");
   res.end("welcome the my life");
 });
-server.listen("3101", () => {
+server.listen(3101, () => {
   console.log("happy to see u again");
 });
